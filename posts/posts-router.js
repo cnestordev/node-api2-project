53a70830cfa6ad5d18505fb1b167215adba06556
Nestor <ncervan87@gmail.com>
2020-09-01 17:26:47 -0700

    commented out redunant post endpoint

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -43,35 +43,35 @@ router.post('/:id/comments', (req, res) => {
 })
 
 // route fully functional but too much unneccesary code
-router.post('/:id/comments', (req, res) => {
-    const id = req.params.id
-    if (req.body.text === '') {
-        res.status(400).json({ errorMessage: 'Please provide text for the comment' })
-    } else {
-        db.findById(id)
-            .then(response => {
-                if (response.length === 0) {
-                    res.status(404).json({ message: 'The post with the specified ID does not exist' })
-                } else {
-                    const newComment = {
-                        text: req.body.text,
-                        post_id: id
-                    }
-                    db.insertComment(newComment)
-                        .then(resp => {
-                            res.status(201).json(newComment)
-                        })
-                        .catch(error => {
-                            console.log(error)
-                            res.status(500).json({ error: 'There was an error while saving the comment to the database' })
-                        })
-                }
-            })
-            .catch(err => {
-                console.log(err)
-            })
-    }
-})
+// router.post('/:id/comments', (req, res) => {
+//     const id = req.params.id
+//     if (req.body.text === '') {
+//         res.status(400).json({ errorMessage: 'Please provide text for the comment' })
+//     } else {
+//         db.findById(id)
+//             .then(response => {
+//                 if (response.length === 0) {
+//                     res.status(404).json({ message: 'The post with the specified ID does not exist' })
+//                 } else {
+//                     const newComment = {
+//                         text: req.body.text,
+//                         post_id: id
+//                     }
+//                     db.insertComment(newComment)
+//                         .then(resp => {
+//                             res.status(201).json(newComment)
+//                         })
+//                         .catch(error => {
+//                             console.log(error)
+//                             res.status(500).json({ error: 'There was an error while saving the comment to the database' })
+//                         })
+//                 }
+//             })
+//             .catch(err => {
+//                 console.log(err)
+//             })
+//     }
+// })
 
 //route fully functional
 router.get('/', (req, res) => {
